98a6fad2d88458d04e4a30a04b177aa0ab3c43c0
vinisi12363 <viniciuspv.si@gmail.com>
2023-04-12 15:02:23 -0300

    mão na massa, implementando algumas funções

--- src/app.js
+++ src/app.js
@@ -4,6 +4,19 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
+import joi from 'joi'
+
+
+const schema = joi.object({
+    username: joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required()
+
+})
+
+
 const api = express()
 api.use(Cors())
 api.use(express.json())
@@ -17,20 +30,74 @@ mongoClient.connect()
 	.then(() => db = mongoClient.db())
 	.catch((err) => console.log(err.message))
 
-api.get("/usuarios", (req, res) => {
+api.get("/participants", (req, res) => {
 	// buscando usuários
 	db.collection("participants").find().toArray()
 		.then(users => res.send(users))  // array de usuários
 		.catch(err => res.status(500).send(err.message))  // mensagem de erro
 });
 
-api.post("/usuarios", (req, res) => {
-	// inserindo usuário
-	db.collection("participants").insertOne({
-		name: "Felipe",
-		lasStatus: 132654565
-	}).then(users => res.sendStatus(201))
-		.catch(err => res.status(500).send(err.message))
+
+function searchUser(name){
+    let userAlreadyExists
+    
+    db.collection("participants").find({name:name}).toArray()
+    .then(users => 
+        console.log ("encontrado"),
+        userAlreadyExists = true
+    ) 
+    .catch(err =>
+        console.log (" nao encontrado"),
+        userAlreadyExists = false
+    ) 
+
+    console.log ("result vale: ", userAlreadyExists)
+  
+    return userAlreadyExists
+
+}
+
+
+api.post("/participants", (req, res) => {
+    let userNameValid
+    let userAlreadyExists
+    const {name} = req.body 
+    if(!name) 
+         return res.status(422).json({error:"o campo de usuario é obrigatorio"})
+
+    try {
+      userNameValid =  schema.validateAsync({username:name})
+    } catch(err) { }
+
+    
+   userAlreadyExists = searchUser(name)   // a função está com um pequeno bug, acredito ser por conta do async
+  
+   if(userAlreadyExists){
+    res.status(409).json({message:"o Nome de usuário já está sendo usado"})
+   }else{
+   
+    if (userNameValid) {
+        db.collection("participants").insertOne({
+            name: name,
+            lasStatus: Date.now()
+        }).then(users => {
+            
+            db.collection("messages").insertOne({
+                from:name,
+                to: "Todos",
+                text:"entra na sala...",
+                type: "status",
+                time: "HH:mm:ss"
+            }).then (mess=> res.sendStatus(201))
+            .catch(err => res.status(500).send(err.message))
+
+
+        })
+            .catch(err => res.status(500).send(err.message))
+
+    }
+   }
+    
 });
 
 
