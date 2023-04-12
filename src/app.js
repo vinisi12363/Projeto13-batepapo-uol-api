6009d3a6affd53936ddd62fa70fa406a062a8521
vinisi12363 <viniciuspv.si@gmail.com>
2023-04-12 12:09:15 -0300

    configurando o banco

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
 import express from 'express'
 import Cors from 'cors'
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const api = express()
-api.use(Cors)
+api.use(Cors())
+api.use(express.json())
 const port = 5000
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+
+mongoClient.connect()
+	.then(() => db = mongoClient.db())
+	.catch((err) => console.log(err.message))
+
+api.get("/usuarios", (req, res) => {
+	// buscando usuários
+	db.collection("participants").find().toArray()
+		.then(users => res.send(users))  // array de usuários
+		.catch(err => res.status(500).send(err.message))  // mensagem de erro
+});
+
+api.post("/usuarios", (req, res) => {
+	// inserindo usuário
+	db.collection("participants").insertOne({
+		email: "Felipe",
+		lasStatus: 132654565
+	}).then(users => res.sendStatus(201))
+		.catch(err => res.status(500).send(err.message))
+});
 
 
 api.listen(port, () => console.log(`Servidor iniciado na porta ${port}`))
